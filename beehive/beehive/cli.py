8f54f7306531f9ebeddb911207946938b255f5e4
Mark Fiers <mark.fiers.42@gmail.com>
2022-07-13 15:11:48 +0200

    Added data command to process & check yaml files

--- beehive/beehive/cli.py
+++ beehive/beehive/cli.py
@@ -7,15 +7,16 @@ from beehive import config
 
 from beehive.app import gene_expression
 from beehive.app import h5ad
+from beehive.app import data
 
 logging.basicConfig(format="%(message)s")
-logging.getLogger('beehive').setLevel(logging.DEBUG)
+logging.getLogger('beehive').setLevel(logging.INFO)
 
 app = typer.Typer()
 
 app.add_typer(gene_expression.app, name="gex")
 app.add_typer(h5ad.app, name="h5ad")
-
+app.add_typer(data.app, name="data")
 
 def run():
     app()
