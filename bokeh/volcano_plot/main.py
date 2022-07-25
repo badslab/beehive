e1734a4ceb44854068d368318747dada3ded8544
r0866691 <raghid.bsat@student.kuleuven.be>
2022-07-25 21:30:38 +0200

    labels, pval lfc true value shows, top5 low5 genes highlighted

--- bokeh/volcano_plot/main.py
+++ bokeh/volcano_plot/main.py
@@ -2,13 +2,14 @@ import logging
 from functools import partial
 import logging
 from pprint import pprint
+from tkinter.tix import TEXT
 import numpy as np
 import pandas as pd
 import polars as pl
 from scipy import stats
 
 from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource,MultiChoice, HoverTool, Spinner, Range1d
+from bokeh.models import ColumnDataSource,MultiChoice, HoverTool, Spinner, Range1d, LabelSet
 from bokeh.models.callbacks import CustomJS
 from bokeh.models.widgets import (Select, TextInput, Div,
                                   Button)
@@ -130,16 +131,24 @@ def modify_data():
 
     data["padj"] = np.log10(data["padj"])*-1
     data["highlight"] = data.apply(lambda x: highlight_genes(x),axis = 1)
+    data["true_padj"] = data["padj"]
+    data["true_lfc"] = data["lfc"]
 
     #take care of x y ranges
     x_range_min = w_x_range.value*-1
     x_range_max = w_x_range.value
     y_range_max = w_y_range.value
-    
+
     ##stick the outliers on the edges
-    data["lfc"] = np.where(data["lfc"] < x_range_min, x_range_min, data["lfc"])
-    data["lfc"] = np.where(data["lfc"] > x_range_max, x_range_max, data["lfc"])
-    data["padj"] = np.where(data["padj"] > y_range_max, y_range_max, data["padj"])
+    data["lfc"] = np.where(data["lfc"] < x_range_min, x_range_min + 0.1, data["lfc"])
+    data["lfc"] = np.where(data["lfc"] > x_range_max, x_range_max - 0.1, data["lfc"])
+    data["padj"] = np.where(data["padj"] > y_range_max, y_range_max - 2, data["padj"])
+
+    #find top genes with extreme values: top 5 high lfc, top 5 low lfc
+    top5 = data.nlargest(5,'lfc')
+    low5 = data.nsmallest(5,'lfc')
+    data.loc[top5.index,"highlight"] = "toplow"
+    data.loc[low5.index,"highlight"] = "toplow"
 
     return data
 
@@ -151,22 +160,40 @@ def get_dataset():
 
 
 dataset_id, dataset = get_dataset()
-tooltips = [
-            ('Log Fold Change:', '@lfc'),
-            ('P-value adjsuted:', '@padj'),
+TOOLTIPS = [
+            ('Log Fold Change:', '@true_lfc'),
+            ('P-value adjsuted:', '@true_padj'),
             ('Gene:', '@gene'),
            ]
 
 
 
 ##Plot###
-plot = figure(height = 400)
-plot.add_tools(HoverTool(tooltips=tooltips))
+plot = figure(height = 400,x_axis_label='Log Fold Change', y_axis_label = '-Log(10) of P-val adjusted')
+
+plot.add_tools(HoverTool(tooltips=TOOLTIPS))
 
 data = modify_data()
 
 source = ColumnDataSource(data)
 
+# ----------------------------------- adjust design options: -----------------------------------
+HIGHLIGHTED_OPTIONS = ["No","Yes","toplow"]
+PALETTE = ["blue","red","green"]
+ALPHAS_PALETTE = [0.01,1,1]
+
+BIG_CIRCLE = 10
+SMALL_CIRCLE = 2
+SIZES_PALETTE_1 = [SMALL_CIRCLE,0,0]
+SIZES_PALETTE_2 = [0,BIG_CIRCLE,BIG_CIRCLE]
+TEXT_SIZES = ['0px','15px','15px']
+
+
+
+#-----------------------------------------------------------------------------------------------
+
+
+##JS helpers to access circle points and edit them.
 v_func_alpha  = """
 var new_xs = new Array(xs.length)
 for(var i = 0; i < xs.length; i++) {
@@ -183,21 +210,58 @@ for(var i = 0; i < xs.length; i++) {
 return new_xs
 """
 
+v_func_text_size  = """
+var new_xs = new Array(xs.length)
+for(var i = 0; i < xs.length; i++) {
+    new_xs[i] = text_sizes_map[xs[i]]
+}
+return new_xs
+"""
 
-alphas = [1,0.01]
-alpha_map = dict(zip(["Yes","No"], alphas))
+#Set up all the design markers
+alpha_map = dict(zip(HIGHLIGHTED_OPTIONS, ALPHAS_PALETTE))
 categorical_alpha_transformer = CustomJSTransform(args={"alpha_map": alpha_map}, v_func=v_func_alpha)
 
-sizes = [10,2]
-size_map = dict(zip(["Yes","No"],sizes))
-categorical_size_transformer =  CustomJSTransform(args={"size_map": size_map},v_func=v_func_size)
+#note the use of two size mappers
+#the first makes the highlighted invisible
+#the second makes the non-highlighted invisible
+#this will ensure highlighted are plotted on top of non-highlighted
+
+size_map_1 = dict(zip(HIGHLIGHTED_OPTIONS,SIZES_PALETTE_1))
+categorical_size_transformer_1 =  CustomJSTransform(args={"size_map": size_map_1},v_func=v_func_size)
+
+size_map_2 = dict(zip(HIGHLIGHTED_OPTIONS,SIZES_PALETTE_2))
+categorical_size_transformer_2 =  CustomJSTransform(args={"size_map": size_map_2},v_func=v_func_size)
+
+text_size_map = dict(zip(HIGHLIGHTED_OPTIONS,TEXT_SIZES))
+categorical_text_size_transformer = CustomJSTransform(args={"text_sizes_map": text_size_map}, v_func = v_func_text_size)
+
 
 plot.scatter(x = "lfc", y = "padj", source = source,
-color=factor_cmap('highlight', palette = ["red","blue"], factors = ["Yes","No"]),
+color=factor_cmap('highlight', palette = PALETTE, factors = HIGHLIGHTED_OPTIONS),
 fill_alpha = transform('highlight',categorical_alpha_transformer),
-size = transform('highlight',categorical_size_transformer))
+size = transform('highlight',categorical_size_transformer_1))
+
+
+
+
+plot.scatter(x = "lfc", y = "padj", source = source,
+color=factor_cmap('highlight', palette = PALETTE, factors = HIGHLIGHTED_OPTIONS),
+fill_alpha = transform('highlight',categorical_alpha_transformer),
+size = transform('highlight',categorical_size_transformer_2))
+
+labels = LabelSet(x='lfc', y='padj', text='gene',
+              x_offset=0, y_offset=-10, source=source,
+              text_font_size = transform('highlight',categorical_text_size_transformer),
+              text_font_style = "bold")
+
+plot.add_layout(labels)
+
+w_x_range.value = max(data["lfc"]) + max(data["lfc"])/2
+w_y_range.value = max(data["padj"]) + max(data["padj"])/4
 
 plot.update(x_range = Range1d(w_x_range.value*-1, w_x_range.value), y_range = Range1d(0, w_y_range.value))
+
 def cb_update_plot(attr, old, new):
     """Populate and update the plot."""
     curdoc().hold()
@@ -229,8 +293,8 @@ def cb_dataset_change(attr, old, new):
     lg.info("Dataset Change to:" + new)
     curdoc().hold()
     update_vars()
-    #update_genes()
-    #update_plot()
+    update_genes()
+    update_plot()
 
 
 def cb_sibling_change(attr, old, new):
@@ -256,8 +320,6 @@ curdoc().add_root(
                     sizing_mode='stretch_width'),
                 row([w_x_range,w_y_range,w_download],
                     sizing_mode='stretch_width')],   
-                # row([w_download,w_x_axis,w_y_axis,w_regression],
-                #     sizing_mode='stretch_width')],   
                 sizing_mode='stretch_width')],
             sizing_mode='stretch_width'),
         row([w_div_title_author],
