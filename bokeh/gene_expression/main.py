46c0d22456f5a61817b621766d87d40aa1d9b7ca
r0866691 <raghid.bsat@student.kuleuven.be>
2022-07-26 15:32:39 +0200

    gene expr ready

--- bokeh/gene_expression/main.py
+++ bokeh/gene_expression/main.py
@@ -190,7 +190,6 @@ plot = figure(background_fill_color="#efefef", x_range=[],
 # plot = figure(background_fill_color="#efefef", x_range=[],
 #               plot_height=400, title="Plot",
 #               toolbar_location='right')
-
 source = ColumnDataSource(get_data())
 table = DataTable(source=source,
                   margin=10,
