5722981221e2477e93bcc8d36571ef48c6a7b20e
r0866691 <raghid.bsat@student.kuleuven.be>
2022-07-28 23:27:23 +0200

    added group by gene expression, colorbar missing still

--- bokeh/scatter_expression/main.py
+++ bokeh/scatter_expression/main.py
@@ -5,13 +5,14 @@ import pandas as pd
 from scipy import stats
 import numpy as np
 from bokeh.layouts import column, row,layout
-from bokeh.models import ColumnDataSource,CheckboxGroup, RadioGroup, Slope
+from bokeh.models import ColumnDataSource,CheckboxGroup, RadioGroup, Slope, LinearColorMapper, ColorBar
 from bokeh.models.callbacks import CustomJS
 from bokeh.models.widgets import (Select, Div,
                                   Button, AutocompleteInput)
 from bokeh.plotting import figure, curdoc
 from bokeh.transform import factor_cmap
 from bokeh.palettes import Category20
+
 
 from beehive import config, util, expset
 # import traceback
@@ -97,16 +98,19 @@ FIXED_OPTIONS = [("geneX","Gene X"),("geneY","Gene Y"),("num_facetX","Numerical 
 # w_y_axis =  create_widget("y_axis",Select, 
 #                         options=FIXED_OPTIONS, title="Select Y-Axis",default = 'geneY')
 
-LABELS = ["Gene","Numerical Facet"]
+LABELS_AXIS = ["Gene","Numerical Facet"]
+LABELS_GROUPING = ["facet","gene"]
 
-w_x_axis_radio = create_widget("x_axis_radio",RadioGroup,labels = LABELS, default = 0, title = "X-Axis",value_type = int)
-w_y_axis_radio = create_widget("y_axis_radio",RadioGroup,labels = LABELS, default = 0, title = "Y-Axis", value_type = int)
+w_x_axis_radio = create_widget("x_axis_radio",RadioGroup,labels = LABELS_AXIS, default = 0, title = "X-Axis",value_type = int)
+w_y_axis_radio = create_widget("y_axis_radio",RadioGroup,labels = LABELS_AXIS, default = 0, title = "Y-Axis", value_type = int)
 
 #categorical facet grouping of data... or gene3 grouping
 w_facet = create_widget("facet", Select, options=[], title="Group by Categories")
 
 w_gene3 = create_widget("gene_categorical", AutocompleteInput,
-                       completions=[], default='APOE', title="Group by Gene Expression", case_sensitive = False)
+                       completions=[], default="APOE", title="Group by Gene Expression", case_sensitive = False)
+
+w_category_radio = create_widget("category_radio",RadioGroup,labels = LABELS_GROUPING, default = 0, title = "Grouped:",value_type = int)
 
 #download button..
 w_download = Button(label='Download', align='end')
@@ -134,6 +138,7 @@ def update_genes():
     genes = get_genes()
     w_gene1.completions = genes
     w_gene2.completions = genes
+    w_gene3.completions = genes
     if w_gene1.value not in genes:
         if 'APOE' in genes:
             w_gene1.value = 'APOE'
@@ -186,9 +191,11 @@ def get_data() -> pd.DataFrame:
     geneY = None
     num_facetX = None
     num_facetY = None
+    geneZ = None
     x_axis = w_x_axis_radio.active
     y_axis = w_y_axis_radio.active
-    
+    gene3 = w_gene3.value
+
     if x_axis == GENE_OPTION:
         geneX = expset.get_gene(dataset_id, gene1)[:,0]
     else:
@@ -198,6 +205,9 @@ def get_data() -> pd.DataFrame:
         geneY = expset.get_gene(dataset_id, gene2)[:,0]
     else:
         num_facetY = expset.get_meta(dataset_id,num_facet2,raw=True)[:,0]
+
+    if gene3:
+        geneZ = expset.get_gene(dataset_id, gene3)[:,0]
 
     ##TODO maybe check for numerical/categorical here?
     lg.warning(f"!! Getting data for {dataset_id} {facet} {gene1}")
@@ -209,9 +219,12 @@ def get_data() -> pd.DataFrame:
         geneY = geneY,
         obs = expset.get_meta(dataset_id, facet)[:,0],
         num_facetX = num_facetX,
-        num_facetY = num_facetY
+        num_facetY = num_facetY,
+        geneZ = geneZ
         ))
     return data
+
+
 
 
 def get_dataset():
@@ -228,8 +241,16 @@ def get_unique_obs(data):
 # Create plot
 #
 
+def get_mapper():
+    mapper = LinearColorMapper(
+    palette='Magma256',
+    low=data["geneZ"].min(),
+    high=data["geneZ"].max())
+    return mapper
+
 plot = figure(output_backend = "webgl")
 data = get_data()
+
 
 unique_obs = get_unique_obs(data)
 dataset_id, dataset = get_dataset()
@@ -241,18 +262,29 @@ index_cmap = factor_cmap('obs', Category20[len(unique_obs)], unique_obs)
 
 X_AXIS = "geneX"
 Y_AXIS = "geneY"
+Z_AXIS = "facet" if w_category_radio.active == 0 else "gene"
 
 ##need to have multiple glyphs not a single multi glyph:
-for index,obs in enumerate(unique_obs):
+if Z_AXIS == "facet":
+    for index,obs in enumerate(unique_obs):
 
-    new_source = ColumnDataSource(data.loc[(data.obs == obs)])
+        new_source = ColumnDataSource(data.loc[(data.obs == obs)])
 
-    plot.scatter(x=X_AXIS, y=Y_AXIS, source=new_source,  legend_label=obs,
-    fill_alpha=0.7, size=5,width=0, fill_color = index_cmap["transform"].palette[index])
+        plot.scatter(x=X_AXIS, y=Y_AXIS, source=new_source,  legend_label=obs,
+        fill_alpha=0.7, size=5,width=0, fill_color = index_cmap["transform"].palette[index])
+    plot.legend.location = "top_right"
+    plot.legend.click_policy = "hide"
+else:
+    mapper = get_mapper()
+    plot.scatter(x=X_AXIS, y=Y_AXIS, source=ColumnDataSource(data),
+            fill_alpha=0.7, size=5,width=0, fill_color = {'field': 'geneZ', 'transform': mapper})
+    # color_bar = ColorBar(color_mapper=mapper, location=(0,0),label_standoff=12,title=w_gene3.value)
+    # plot.add_layout(color_bar,"right")
 
 
-plot.legend.location = "top_right"
-plot.legend.click_policy = "hide"
+
+
+
 x_label = ""
 y_label = ""
 
@@ -269,39 +301,53 @@ def cb_update_plot(attr, old, new, type_change):
     dataset_id, dataset = get_dataset()
 
     facet = w_facet.value
-
+    
     unique_obs = get_unique_obs(data)
 
     #TODO fix None => strings
     index_cmap = factor_cmap('obs', Category20[len(unique_obs)], unique_obs)
-    
+
+    if plot.right:
+        plot.right = []
+
     plot.renderers = []
-    plot.legend.items = []
+    if plot.legend:
+        plot.legend.items = []
 
     X_AXIS = "geneX" if w_x_axis_radio.active == GENE_OPTION else "num_facetX"
     Y_AXIS = "geneY" if w_y_axis_radio.active == GENE_OPTION else "num_facetY"
-    
-    for index,obs in enumerate(unique_obs):
+    Z_AXIS = "facet" if w_category_radio.active == 0 else "gene"
 
-        new_source = ColumnDataSource(data.loc[(data.obs == obs)])
+    if Z_AXIS == "facet":
+        for index,obs in enumerate(unique_obs):
 
-        if len(w_regression.active) == 1:
-            # plot.update(output_backend = "canvas")
-            slope, intercept, r_value, p_value, std_err = stats.linregress(new_source.data[X_AXIS],y = new_source.data[Y_AXIS])
-            y_predicted = [round(slope,3)*i + round(intercept,3)  for i in new_source.data[X_AXIS]]
-           
+            new_source = ColumnDataSource(data.loc[(data.obs == obs)])
 
-            # regression_line = Slope(gradient=slope, y_intercept=intercept, line_color=index_cmap["transform"].palette[index])
-            # plot.add_layout(regression_line)
-            plot.line(new_source.data[X_AXIS],np.array(y_predicted),color=index_cmap["transform"].palette[index],
-            legend_label=f"{obs}: y={str(round(slope,2))}x+{str(round(intercept,2))}, p-value={'{:e}'.format(p_value)}, r-value={str(round(r_value,2))}")
-        else:
-            # plot.update(output_backend = "webgl")
-            plot.scatter(x=X_AXIS, y=Y_AXIS, source=new_source,  legend_label=obs,
-            fill_alpha=0.7, size=5,width=0, fill_color = index_cmap["transform"].palette[index])
+            if len(w_regression.active) == 1:
+                # plot.update(output_backend = "canvas")
+                slope, intercept, r_value, p_value, std_err = stats.linregress(new_source.data[X_AXIS],y = new_source.data[Y_AXIS])
+                y_predicted = [round(slope,3)*i + round(intercept,3)  for i in new_source.data[X_AXIS]]
+            
+                # regression_line = Slope(gradient=slope, y_intercept=intercept, line_color=index_cmap["transform"].palette[index])
+                # plot.add_layout(regression_line)
+                plot.line(new_source.data[X_AXIS],np.array(y_predicted),color=index_cmap["transform"].palette[index],
+                legend_label=f"{obs}: y={str(round(slope,2))}x+{str(round(intercept,2))}, p-value={'{:e}'.format(p_value)}, r-value={str(round(r_value,2))}")
+            else:
+                # plot.update(output_backend = "webgl")
+                plot.scatter(x=X_AXIS, y=Y_AXIS, source=new_source,  legend_label=obs,
+                fill_alpha=0.7, size=5,width=0, fill_color = index_cmap["transform"].palette[index])
+   
+        plot.legend.location = "top_right"
+        plot.legend.click_policy = "hide"
+    else:
+        mapper = get_mapper()
+        plot.scatter(x=X_AXIS, y=Y_AXIS, source=ColumnDataSource(data),
+                fill_alpha=0.7, size=5,width=0, fill_color = {'field': 'geneZ', 'transform': mapper})
+       # plot.rect(x=0.5, y='geneZ', fill_color='color', width=1, height=1, source=source)
 
-    plot.legend.location = "top_right"
-    plot.legend.click_policy = "hide"
+        # color_bar = ColorBar(color_mapper=mapper, location=(0,0),label_standoff=12,title=w_gene3.value)
+        # plot.add_layout(color_bar,"right")
+
 
     w_div_title_author.text = \
         f"""
@@ -359,9 +405,9 @@ cb_download = CustomJS(
 
 
 w_regression.on_change("active",partial(cb_update_plot,type_change = "regression"))
-# w_x_axis.on_change("value",partial(cb_update_plot,type_change = "XYAXIS"))
-# w_y_axis.on_change("value",partial(cb_update_plot,type_change = "XYAXIS"))
 w_facet.on_change("value", partial(cb_update_plot,type_change="categorical"))
+w_gene3.on_change("value",partial(cb_update_plot,type_change="categorical"))
+
 
 w_gene1.on_change("value",partial(cb_update_plot,type_change = "geneX"))
 w_gene2.on_change("value", partial(cb_update_plot,type_change="geneY"))
@@ -370,7 +416,7 @@ w_facet_numerical_2.on_change("value", partial(cb_update_plot,type_change="num_f
 
 w_x_axis_radio.on_change("active",partial(cb_update_plot,type_change="XYAXIS"))
 w_y_axis_radio.on_change("active",partial(cb_update_plot,type_change="XYAXIS"))
-
+w_category_radio.on_change("active",partial(cb_update_plot,type_change="categorical"))
 
 # w_sibling.on_change("value", cb_sibling_change)
 w_dataset_id.on_change("value", cb_dataset_change)
@@ -388,14 +434,13 @@ curdoc().add_root(
                     sizing_mode='stretch_width'),
                 row([w_x_axis_radio,w_y_axis_radio],
                     sizing_mode='stretch_width'),
-                row([w_gene3,w_facet,w_download],
+                row([w_gene3,w_facet,w_download,w_category_radio],
                     sizing_mode='stretch_width')],   
                 sizing_mode='stretch_width')],
             sizing_mode='stretch_width'),
         row([w_div_title_author],
             sizing_mode='stretch_width'),
         #TODO regression doesnt work with webgl
-        # row([w_regression]),
         row([w_gene_not_found],
             sizing_mode='stretch_width'),
         row([plot],
