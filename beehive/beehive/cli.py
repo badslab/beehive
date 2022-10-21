e2f78d300e324fc887980c96a472db40e00e2751
Mark Fiers <mark.fiers.42@gmail.com>
2022-10-21 10:06:03 +0200

    added geneset to the CLI

--- beehive/beehive/cli.py
+++ beehive/beehive/cli.py
@@ -8,6 +8,7 @@ from beehive import config
 from beehive.app import gene_expression
 from beehive.app import h5ad
 from beehive.app import data
+from beehive.app import geneset
 
 logging.basicConfig(format="%(message)s")
 logging.getLogger('beehive').setLevel(logging.INFO)
@@ -17,6 +18,7 @@ app = typer.Typer()
 app.add_typer(gene_expression.app, name="gex")
 app.add_typer(h5ad.app, name="h5ad")
 app.add_typer(data.app, name="data")
+app.add_typer(geneset.app, name="geneset")
 
 
 def run():
