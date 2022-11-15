990f4d8650ce43a6d7188c8298568e5f17f77ce9
Mark Fiers <mark.fiers.42@gmail.com>
2022-11-15 07:25:14 +0100

    autorestart in case of crash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-bokeh serve --allow-websocket-origin=${VISUALIZATION_WEBSOCKET_ORIGIN} \
-			 --port ${PORT_FOR_VISUALIZATION} \
-			bokeh/gene_expression/ \
-			bokeh/volcano_plot/ \
-			bokeh/scatter_expression/ \
-            bokeh/hexbin_expression/ \
-            bokeh/quadrant_plot/ 
+while true; do
+    echo "Start bokeh"
+    bokeh serve \
+    --allow-websocket-origin=${VISUALIZATION_WEBSOCKET_ORIGIN} \
+    --port ${PORT_FOR_VISUALIZATION} \
+    bokeh/gene_expression/ \
+    bokeh/volcano_plot/ \
+    bokeh/scatter_expression/ \
+    bokeh/hexbin_expression/ \
+    bokeh/quadrant_plot/ \
+    bokeh/gsea_view/
+    echo "Crash?"
+    echo "-------------------------------------"
+    sleep 1
+done
