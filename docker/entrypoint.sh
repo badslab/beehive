3e063d7960679139df28a45f7ca328801457e25e
Mark Fiers <mark.fiers.42@gmail.com>
2022-12-05 17:23:46 +0100

    restructured docker images

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
+
+set -v
+
+echo "Starting Docker Entrypoint"
+
+# start nginx to serve static pages & as a reverse proxy
+echo "Start nginx"
+nginx -c /beehive/docker/nginx.conf
+
+
+echo "Go in infinite bokeh loop"
 while true; do
-    echo "Start bokeh"
+    echo "Starting bokeh"
     bokeh serve \
     --allow-websocket-origin=${VISUALIZATION_WEBSOCKET_ORIGIN} \
-    --port ${PORT_FOR_VISUALIZATION} \
+    --port 5009 \
     bokeh/gene_expression/ \
     bokeh/volcano_plot/ \
     bokeh/scatter_expression/ \
     bokeh/hexbin_expression/ \
     bokeh/quadrant_plot/ \
     bokeh/gsea_view/
-    echo "Crash?"
+    echo "Crashed? That's not good..."
     echo "-------------------------------------"
     sleep 1
 done
