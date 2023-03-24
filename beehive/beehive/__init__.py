c1f1f7a95bcf85f9e31138608f8131efe9706650
Mark Fiers <mark.fiers.42@gmail.com>
2023-03-24 10:13:28 +0100

    minor updates

--- beehive/beehive/__init__.py
+++ beehive/beehive/__init__.py
@@ -1,14 +1,12 @@
 
 import os
-
 from pathlib import Path
-
 
 BASEDIR = Path(os.environ['BEEHIVE_BASEDIR'])
 # BASEDIR = Path(__file__).parent.parent.parent
-BOKEHDIR = BASEDIR / 'bokeh'
+#BOKEHDIR = BASEDIR / 'bokeh'
 
-if 'BEEHIVE_DATADIR' in os.environ:
-    DATADIR = Path(os.environ['BEEHIVE_DATADIR'])
-else:
-    DATADIR = BASEDIR / 'data'
+# if 'BEEHIVE_DATADIR' in os.environ:
+#     DATADIR = Path(os.environ['BEEHIVE_DATADIR'])
+# else:
+DATADIR = BASEDIR / 'data'
